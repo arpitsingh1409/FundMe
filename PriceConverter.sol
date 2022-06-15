1499721a77bd42e6de6192c099d584719d0035a6
Arpit Singh <f20190956@pilani.bits-pilani.ac.in>
2022-06-16 04:25:29 +0530

    Create PriceConverter.sol

---
+++ PriceConverter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    
+    function getPrice() internal view returns (uint256) {
+    
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        
+        return uint256(answer * 1e10);
+    }
+
+    // 1000000000
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
